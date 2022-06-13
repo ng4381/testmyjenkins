e45f52b5755261daf2ea59e74b6dc8a46ff29729
Nikita Goloveshko <ng4381@mail.ru>
2022-06-13 11:34:35 +0300

    -

--- src/main/java/com/example/testjenkins/TestjenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestjenkinsApplication.java
@@ -2,9 +2,16 @@ package com.example.testjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestjenkinsApplication {
+public class TestjenkinsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestjenkinsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestjenkinsApplication.class, args);
